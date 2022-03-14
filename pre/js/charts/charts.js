36211eb08e07dd2a4cb36a4f78c02efedb99b575
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-03-14 12:06:22 +0100

    Nuevo cambio en datos

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -19,15 +19,9 @@ COLOR_GREY_2 = '#64605A',
 COLOR_OTHER_1 = '#B58753', 
 COLOR_OTHER_2 = '#731854';
 
-export function initChart1_10(iframe) {
-    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
-    document.getElementById('title').textContent = 'Figura 1.9. Distribución de la población extranjera por nacionalidad en España, 2021';
-    document.getElementById('subtitle').textContent = 'Comparativa entre la población total y la población con 65 y más años. Datos en valores porcentuales.';
-    document.getElementById('data-source').textContent = 'Instituto Nacional de Estadística (INE): Estadística del Padrón continuo a 1 de enero de 2021. Consulta: febrero de 2022.';
-    document.getElementById('data-note').textContent = "No se incluye Reino Unido dentro de la Unión Europea. En 'Otros' se encuentran: Oceanía, Centro América, Caribe y Apátridas";
-
+export function initChart(iframe) {
     //Desarrollo del gráfico
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe-perfil-mayores-2022-demografia/main/data/distribucion_poblacion_extranjeros.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_demografia_1_10/main/data/distribucion_poblacion_extranjeros.csv', function(error,data) {
         if (error) throw error;
 
         console.log(data);
@@ -43,7 +37,7 @@ export function initChart1_10(iframe) {
         });
 
         //Iframe
-        setFixedIframeUrl('distribucion_poblacion_mayor_extranjera');
+        setFixedIframeUrl('informe_perfil_mayores_2022_demografia_1_10','distribucion_poblacion_mayor_extranjera');
 
         //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
         setRRSSLinks('distribucion_poblacion_mayor_extranjera');
@@ -62,50 +56,4 @@ export function initChart1_10(iframe) {
         //Altura del frame
         setChartHeight(iframe);
     });    
-}
-
-export function initChart1_11(iframe) {
-    //Desarrollo de funciones asociadas al gráfico > Título, subtítulo, notas, fuente de datos
-    document.getElementById('title').textContent = 'Figura 1.10. Comparativa de pirámides de población nacional y extranjera en España por nacionalidad, 2021';
-    document.getElementById('subtitle').textContent = 'Datos en valores porcentuales.';
-    document.getElementById('data-source').textContent = 'Instituto Nacional de Estadística (INE): Estadística del Padrón continuo a 1 de enero de 2021. Consulta: febrero de 2022.';
-    document.getElementById('data-note').textContent = '';
-
-    //Desarrollo del gráfico
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe-perfil-mayores-2022-demografia/main/data/piramide_extranjeros_nacional.csv', function(error,data) {
-        if (error) throw error;
-
-        /////
-        /////
-        // Resto
-        /////
-        /////
-        //Animación del gráfico
-        document.getElementById('replay').addEventListener('click', function() {
-            animateChart();
-        });
-
-        //Iframe
-        setFixedIframeUrl('piramide_espanoles_extranjeros');
-
-        //Redes sociales > Antes tenemos que indicar cuál sería el texto a enviar
-        setRRSSLinks('piramide_espanoles_extranjeros');
-
-        //Captura de pantalla de la visualización
-        setChartCanvas();
-        setCustomCanvas();
-
-        let pngDownload = document.getElementById('pngImage');
-
-        pngDownload.addEventListener('click', function(){
-            setChartCanvasImage('piramide_espanoles_extranjeros');
-            setChartCustomCanvasImage('piramide_espanoles_extranjeros');
-        });
-
-        //Altura del frame
-        setChartHeight(iframe);
-    }); 
-
-
-    
 }
--- pre/js/main.js
+++ pre/js/main.js
@@ -1,6 +1,6 @@
 import 'url-search-params-polyfill';
 import './modules/tabs';
-import * as charts from './charts/charts';
+import { initChart } from './charts/charts';
 
 //Necesario para importar los estilos de forma automática en la etiqueta 'style' del html final
 import '../css/main.scss';
@@ -14,59 +14,7 @@ function getUrlParams() {
     const params = new URLSearchParams(window.location.search);
 
     //Visualización y tipo de iframe
-    const viz = params.get('chart');
     const iframe = params.get('iframe');
 
-    setChart(viz, iframe);
-}
-
-function setChart(viz, iframe) {
-    pymChild.onMessage('viewport-iframe-position', onScroll);    
-
-    function onScroll(parentInfo) {
-        let steps = parentInfo.split(" ");
-
-        if(steps[2] != 0 && !isChartActive) {
-            switch(viz){
-                //Demografía
-                case '1_1':
-                    charts.initChart1_1(iframe, steps[2]);
-                    break;
-                case '1_2':
-                    charts.initChart1_2(iframe, steps[2]);
-                    break;
-                case '1_3':
-                    charts.initChart1_3(iframe, steps[2]);
-                    break;
-                case '1_4':
-                    charts.initChart1_4(iframe, steps[2]);
-                    break;
-                case '1_5':
-                    charts.initChart1_5(iframe, steps[2]);
-                    break;
-                case '1_6':
-                    charts.initChart1_6(iframe, steps[2]);
-                    break;
-                case '1_7':
-                    charts.initChart1_7(iframe, steps[2]);
-                    break;
-                case '1_8':
-                    charts.initChart1_8(iframe, steps[2]);
-                    break;
-                case '1_9':
-                    charts.initChart1_9(iframe, steps[2]);
-                    break;
-                case '1_10':
-                    charts.initChart1_10(iframe, steps[2]);
-                    break;
-                case '1_11':
-                    charts.initChart1_11(iframe, steps[2]);
-                    break;
-                default: //Si no hay parámetros en la URL
-                    charts.initChart1_1('responsive', steps[2]);
-                        break;                
-            }
-            isChartActive = true;
-        }
-    }    
+    initChart(iframe);
 }
