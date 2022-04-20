e61a1f8ec8e0a32b8cab931cb8edcc8c12452887
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-20 13:17:29 +0200

    Primer desarrollo serio para web terminado

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,6 +1,6 @@
 //Desarrollo de las visualizaciones
 import * as d3 from 'd3';
-import { numberWithCommas2 } from '../helpers';
+import { numberWithCommas3 } from '../helpers';
 import { getInTooltip, getOutTooltip, positionTooltip } from '../modules/tooltip';
 import { setChartHeight } from '../modules/height';
 import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
@@ -8,24 +8,35 @@ import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
-const COLOR_PRIMARY_1 = '#F8B05C', 
-COLOR_PRIMARY_2 = '#E37A42',
+const COLOR_PRIMARY_1 = '#F8B05C',
 COLOR_COMP_1 = '#528FAD', 
 COLOR_COMP_2 = '#AADCE0',
-COLOR_GREY_1 = '#D6D6D6', 
-COLOR_GREY_2 = '#A3A3A3',
-COLOR_ANAG__PRIM_1 = '#BA9D5F', 
+COLOR_GREY_1 = '#D6D6D6',
+COLOR_ANAG_PRIM_1 = '#BA9D5F', 
 COLOR_ANAG_PRIM_2 = '#9E6C51',
-COLOR_ANAG_PRIM_3 = '#9E3515',
 COLOR_ANAG_COMP_1 = '#1C5A5E';
+let tooltip = d3.select('#tooltip');
+
+//Diccionario
+let dictionary = {
+    UE27_2020: 'Unión Europea',
+    EU_NO_UE27: 'Resto Europa',
+    AF: 'África',
+    AN: 'América del Norte',
+    SUD: 'Sudamérica',
+    AS: 'Asia',
+    OT_2: 'Otros'
+}
 
 export function initChart(iframe) {
     //Desarrollo del gráfico
     d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_demografia_1_9/main/data/distribucion_poblacion_extranjeros_v2.csv', function(error,data) {
         if (error) throw error;
+
+        data = [data[0],data[1]]
         
         //Declaramos fuera las variables genéricas
-        let margin = {top: 20, right: 20, bottom: 20, left: 85},
+        let margin = {top: 5, right: 15, bottom: 20, left: 62.5},
             width = document.getElementById('chart').clientWidth - margin.left - margin.right,
             height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
 
@@ -54,7 +65,7 @@ export function initChart(iframe) {
             .domain(['todas','mas65'])
             .range([0, height]);
         
-        let tickLabels = ['Total', '65 y más años'];
+        let tickLabels = ['Total', '65 y más'];
 
         let yAxis = function(g) {
             g.call(d3.axisLeft(y).tickFormat((d,i) => tickLabels[i]))
@@ -66,7 +77,7 @@ export function initChart(iframe) {
 
         let color = d3.scaleOrdinal()
             .domain(gruposContinentes)
-            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_GREY_1, COLOR_OTHER_1, COLOR_OTHER_2]);
+            .range([COLOR_PRIMARY_1, COLOR_ANAG_PRIM_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_ANAG_COMP_1, COLOR_ANAG_PRIM_2, COLOR_GREY_1]);
 
         let stackedDataContinentes = d3.stack()
             .keys(gruposContinentes)
@@ -80,15 +91,57 @@ export function initChart(iframe) {
                 .enter()
                 .append("g")
                 .attr("fill", function(d) { return color(d.key); })
+                .attr('class', function(d) {
+                    return 'rect-1 ' + d.key;
+                })
                 .selectAll("rect")
                 .data(function(d) { return d; })
                 .enter()
                 .append("rect")
-                .attr('class','rect')
+                .attr('class', 'rect')
                 .attr("y", function(d) { return y(d.data.grupo) + y.bandwidth() / 4; })
                 .attr("x", function(d) { return 0; })
                 .attr("width", function(d) { return x(0); })
                 .attr("height", y.bandwidth() / 2)
+                .on('mouseover', function(d,i,e){
+                    console.log(d, data);
+                    
+
+                    //Opacidad de las barras
+                    let other = svg.selectAll('.rect-1');
+                    let current = this.parentNode.classList[1];
+                    let _this = svg.selectAll(`.${current}`);
+                    
+                    other.each(function() {
+                        this.style.opacity = '0.4';
+                    });
+                    _this.each(function() {
+                        this.style.opacity = '1';
+                    });
+
+                    console.log(current);
+
+                    //Texto                    
+                    let html = '<p class="chart__tooltip--title">' + dictionary[current] + '</p>' + 
+                        '<p class="chart__tooltip--text">Entre la población extranjera en España, este conjunto de nacionalidades representa un <b>' + numberWithCommas3(parseFloat(data[1][current]).toFixed(2)) + '%</b> a nivel general y un <b>' + numberWithCommas3(parseFloat(data[0][current]).toFixed(2)) + '%</b> entre las personas con 65 o más años</p>';
+            
+                    tooltip.html(html);
+
+                    //Tooltip
+                    positionTooltip(window.event, tooltip);
+                    getInTooltip(tooltip);
+
+                })
+                .on('mouseout', function(d,i,e) {
+                    //Quitamos los estilos de la línea
+                    let bars = svg.selectAll('.rect-1');
+                    bars.each(function() {
+                        this.style.opacity = '1';
+                    });
+                
+                    //Quitamos el tooltip
+                    getOutTooltip(tooltip); 
+                })
                 .transition()
                 .duration(2000)
                 .attr("x", function(d) { return x(d[0]); })
